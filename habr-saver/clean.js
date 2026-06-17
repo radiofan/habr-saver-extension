cd6df570491eca5ed3b28e6027d634f3929f22b3
RADIOFAN <radiofan22@mail.ru>
2026-06-17 11:30:48 +0700

    fix: add checking event listener hook

--- habr-saver/clean.js
+++ habr-saver/clean.js
@@ -1,7 +1,13 @@
 (function(){
 	console.log("clean habr");
-
-	window.__eventListenerCleaner.openSpoilers();
-	window.__eventListenerCleaner.removeAll();
-	window.__eventListenerCleaner.cleanElements();
+	
+	if(!window.__eventListenerCleaner.hasOwnProperty('openSpoilers') ||
+		!window.__eventListenerCleaner.hasOwnProperty('removeAll') ||
+		!window.__eventListenerCleaner.hasOwnProperty('cleanElements')){
+		alert("Need to reload page for hooking event listeneres");
+	}else{
+		window.__eventListenerCleaner.openSpoilers();
+		window.__eventListenerCleaner.removeAll();
+		window.__eventListenerCleaner.cleanElements();
+	}
 })();
