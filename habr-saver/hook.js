a3ea1f05c6bce4090d4ac8e96673692abd9b3bfd
RADIOFAN <radiofan22@mail.ru>
2026-06-17 11:31:47 +0700

    fix: add more removable blocks

--- habr-saver/hook.js
+++ habr-saver/hook.js
@@ -43,11 +43,19 @@
 			listeners.length = 0;
 		},
 		cleanElements(){
+			// header and sidebar
 			for(el of document.querySelectorAll("div.header-banner-wrapper")){ el.remove();}
 			for(el of document.querySelectorAll("header.tm-header.tm-header.tm-header_with-feature")){ el.remove();}
-			for(el of document.querySelectorAll("div.tm-footer-menu")){ el.remove();}
-			for(el of document.querySelectorAll("div.tm-footer")){ el.remove();}
 			for(el of document.querySelectorAll("#overlays")){ el.remove();}
+			for(el of document.querySelectorAll("div.container-wrapper div.inner.single-banner")){ el.remove();}
+			for(el of document.querySelectorAll("div.tm-page__sidebar")){ el.remove();}
+			
+			// comments
+			for(el of document.querySelectorAll("#publication-comments div.tm-comment-form")){ el.remove();}
+			for(el of document.querySelectorAll("#publication-comments div.tm-comment-navigation.tm-comment-navigation__block.tm-comment-navigation__has-new_reverse")){ el.remove();}
+			for(el of document.querySelectorAll("#publication-comments div.tm-notice.tm-notice_positive.tm-comments__comment-notice")){ el.remove();}
+			
+			// footer
 			for(el of document.querySelectorAll("div.tm-article-presenter__footer section.tm-block.tm-block_spacing-bottom")){
 				if(el.querySelector(".article-author")){
 					continue;
@@ -55,20 +63,26 @@
 				el.remove();
 			}
 			for(el of document.querySelectorAll("div.tm-article-presenter__footer section.tm-block.tm-block_spacing-around")){ el.remove();}
-			for(el of document.querySelectorAll("div.container-wrapper div.inner.single-banner")){ el.remove();}
 			for(el of document.querySelectorAll("div.tm-article-presenter__footer div.tm-project-block.tm-project-block--courses")){ el.remove();}
 			for(el of document.querySelectorAll("div.tm-article-presenter__footer div.tm-project-block.tm-project-block--salary")){ el.remove();}
+			for(el of document.querySelectorAll("div.tm-article-presenter__footer div.tm-project-block.tm-project-block--vacancies")){ el.remove();}
+			for(el of document.querySelectorAll("div.tm-footer-menu")){ el.remove();}
+			for(el of document.querySelectorAll("div.tm-footer")){ el.remove();}
+			
+			// different
 			for(el of document.querySelectorAll("script")){ el.remove();}
 			for(el of document.querySelectorAll("noscript")){ el.remove();}
-			for(el of document.querySelectorAll("#publication-comments div.tm-comment-form")){ el.remove();}
-			for(el of document.querySelectorAll("#publication-comments div.tm-comment-navigation.tm-comment-navigation__block.tm-comment-navigation__has-new_reverse")){ el.remove();}
-			for(el of document.querySelectorAll("div.tm-page__sidebar")){ el.remove();}
-			for(el of document.querySelectorAll("div.tm-page-progress-bar")){ el.remove();}
-			for(el of document.querySelectorAll("button.tm-scroll-top")){ el.remove();}
-			for(el of document.querySelectorAll("div.pull-down__header")){ el.remove();}
-			for(el of document.querySelectorAll("div.sponsor-block")){ el.remove();}
 			for(el of document.querySelectorAll("img[aria-hidden=\"true\"]")){ el.remove();}
 			for(el of document.querySelectorAll(".code-explainer")){ el.remove();}
+			for(el of document.querySelectorAll("div.pull-down__header")){ el.remove();}
+			for(el of document.querySelectorAll("div.tm-page-progress-bar")){ el.remove();}
+			for(el of document.querySelectorAll("button.tm-scroll-top")){ el.remove();}
+			for(el of document.querySelectorAll("div.sponsor-block")){ el.remove();}
+			for(el of document.querySelectorAll("div.article-poll-container > div.article-poll > div.tm-notice.tm-notice_positive.notice")){ el.remove();}
+			for(el of document.querySelectorAll("div.digest-subscription")){ el.remove();}
+			
+			
+			// styling
 			for(el of document.querySelectorAll("details > summary")){ el.style.display ="none";}
 			document.querySelector("main > div.tm-page").style.padding = "0";
 			for(el of document.querySelectorAll(".spoiler__content")){ el.style.padding = "2px"; el.style.margin = "0";}
